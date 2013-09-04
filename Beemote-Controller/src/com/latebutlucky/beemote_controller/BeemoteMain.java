d3afb7abef59a6ad18a4b1a5a7bd2d4550f7bdae
KwonSub <kingkimgs@nate.com>
2013-09-04 17:32:36 +0900

    xml로 view 관리

--- Beemote-Controller/src/com/latebutlucky/beemote_controller/BeemoteMain.java
+++ Beemote-Controller/src/com/latebutlucky/beemote_controller/BeemoteMain.java
@@ -1,11 +1,10 @@
 package com.latebutlucky.beemote_controller;
 
-import java.io.IOException;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -14,8 +13,6 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
-
-import com.lge.tv.a2a.client.A2AClientManager;
 
 public class BeemoteMain extends Activity {
 
@@ -27,35 +24,22 @@ public class BeemoteMain extends Activity {
 		StrictMode.enableDefaults();
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mainview);
+		
+		LayoutInflater inflater = getLayoutInflater();
+		View v = inflater.inflate(R.layout.activity_beemote, null);
+		addContentView(v, new RelativeLayout.LayoutParams(480, 800));
+		
 		SlidingView sv = new SlidingView(this);
 		for (int i = 0; i < bv.length; i++) {
 			bv[i] = new BeeView(this);
-		}
-
-		FrameLayout frm = new FrameLayout(this);
-		RelativeLayout rlRelativeLayout = new RelativeLayout(this);
-		RelativeLayout.LayoutParams lpRelativeLayout_Property = new RelativeLayout.LayoutParams(
-				ViewGroup.LayoutParams.MATCH_PARENT,
-				ViewGroup.LayoutParams.MATCH_PARENT);
-		rlRelativeLayout.setLayoutParams(lpRelativeLayout_Property);
-
-		btnTvList = new Button(this);
-		RelativeLayout.LayoutParams btn_Property = new RelativeLayout.LayoutParams(
-				ViewGroup.LayoutParams.WRAP_CONTENT,
-				ViewGroup.LayoutParams.WRAP_CONTENT);
-		btnTvList.setText("CH+");
-		btn_Property.topMargin = 300;
-		btn_Property.leftMargin = 100;
-		btnTvList.setLayoutParams(btn_Property);
-		rlRelativeLayout.addView(btnTvList);
-
-		for (int i = 0; i < bv.length; i++) {
 			sv.addView(bv[i]);
-		}
-		frm.addView(sv);
-		frm.addView(rlRelativeLayout);
-		setContentView(frm);
-
+		}	
+		
+		FrameLayout framelayout = (FrameLayout) findViewById(R.id.main_view);
+		framelayout.addView(sv);
+		
+		btnTvList = (Button) findViewById(R.id.btnTVList);
 		btnTvList.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -67,32 +51,32 @@ public class BeemoteMain extends Activity {
 			}
 		});
 
-//		Button btnTouchPad = (Button) findViewById(R.id.btnTouchPad);
-//		btnTouchPad.setOnClickListener(new OnClickListener() {
-//
-//			@Override
-//			public void onClick(View v) {
-//				// TODO Auto-generated method stub
-//				Intent intent = new Intent(getApplicationContext(),
-//						TouchPad.class);
-//				startActivity(intent);
-//			}
-//		});
-//
-//		Button btnUp = (Button) findViewById(R.id.btnup);
-//		btnUp.setOnClickListener(new OnClickListener() {
-//
-//			@Override
-//			public void onClick(View v) {
-//				// TODO Auto-generated method stub
-//				try {
-//					A2AClientManager.getDefaultClient().handleKey();
-//				} catch (IOException e) {
-//					// TODO Auto-generated catch block
-//					e.printStackTrace();
-//				}
-//			}
-//		});
+		// Button btnTouchPad = (Button) findViewById(R.id.btnTouchPad);
+		// btnTouchPad.setOnClickListener(new OnClickListener() {
+		//
+		// @Override
+		// public void onClick(View v) {
+		// // TODO Auto-generated method stub
+		// Intent intent = new Intent(getApplicationContext(),
+		// TouchPad.class);
+		// startActivity(intent);
+		// }
+		// });
+		//
+		// Button btnUp = (Button) findViewById(R.id.btnup);
+		// btnUp.setOnClickListener(new OnClickListener() {
+		//
+		// @Override
+		// public void onClick(View v) {
+		// // TODO Auto-generated method stub
+		// try {
+		// A2AClientManager.getDefaultClient().handleKey();
+		// } catch (IOException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+		// }
+		// });
 
 	}
 
